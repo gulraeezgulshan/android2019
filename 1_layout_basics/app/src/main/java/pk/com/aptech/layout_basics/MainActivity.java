de6636b396ebd492c6c31c62e212ad52ffbf8103
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2019-10-25 00:05:47 +0500

    first class basics

---
+++ 1_layout_basics/app/src/main/java/pk/com/aptech/layout_basics/MainActivity.java
@@ -0,0 +1,13 @@
+package pk.com.aptech.layout_basics;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
