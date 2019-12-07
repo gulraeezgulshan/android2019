1ea018e7fb14b56a02ec80bfd93f25877eb51d68
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2019-12-07 19:56:56 +0500

    update content providers

--- ContentProviders/CustomContentProvider/app/src/main/java/pk/com/aptech/customcontentprovider/MainActivity.java
+++ ContentProviders/CustomContentProvider/app/src/main/java/pk/com/aptech/customcontentprovider/MainActivity.java
@@ -1,8 +1,15 @@
 package pk.com.aptech.customcontentprovider;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAddTitle(View view) {
+    //---add a book---
+        ContentValues values = new ContentValues();
+        values.put(BooksProvider.TITLE, ((EditText)
+                findViewById(R.id.txtTitle)).getText().toString());
+        values.put(BooksProvider.ISBN, ((EditText)
+                findViewById(R.id.txtISBN)).getText().toString());
+        Uri uri = getContentResolver().insert(
+                BooksProvider.CONTENT_URI, values);
+        Toast.makeText(getBaseContext(),uri.toString(),
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickRetrieveTitles(View view) {
+    //---retrieve the titles---
+        Uri allTitles = Uri.parse(
+                "content://pk.com.aptech.provider.Books/books");
+        Cursor c;
+        CursorLoader cursorLoader = new CursorLoader(
+                this,
+                allTitles, null, null, null,
+                "title desc");
+        c = cursorLoader.loadInBackground();
+        if (c.moveToFirst()) {
+            do {
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(
+                                BooksProvider._ID)) + ", " +
+                                c.getString(c.getColumnIndex(
+                                        BooksProvider.TITLE)) + ", " +
+                                c.getString(c.getColumnIndex(
+                                        BooksProvider.ISBN)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
     }
 }
