8c4d47c827750b3cc66f30ce1e5ae477e76ffebd
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2019-12-19 16:08:11 +0500

    firebase update

--- Firebase/FirstApp/app/src/main/java/pk/com/aptech/firstapp/MainActivity.java
+++ Firebase/FirstApp/app/src/main/java/pk/com/aptech/firstapp/MainActivity.java
@@ -13,12 +13,14 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnSave;
     Button btnFetch;
+    Button btnUpdate;
+    Button btnDelete;
     EditText txtName;
     TextView lblStatus;
 
@@ -42,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
 
         btnSave = (Button) findViewById(R.id.btnSave);
         btnFetch = (Button) findViewById(R.id.btnFetch);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
         lblStatus = (TextView)findViewById(R.id.lblStatus);
         txtName = (EditText) findViewById(R.id.txtValue);
 
@@ -50,9 +56,19 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //String value = dataSnapshot.getValue(String.class);
 
-                Map<String,Object> data = (Map<String,Object>)dataSnapshot.getValue();
+               /* Map<String,Object> data = (Map<String,Object>)dataSnapshot.getValue();
                 String values = data.get("age").toString() + " : " + data.get("name").toString();
-                lblStatus.setText(values);
+                lblStatus.setText(values);*/
+
+               //Reading multiple children
+
+                for (DataSnapshot snapshot: dataSnapshot.getChildren())
+                {
+                    Map<String,Object> data = (Map<String,Object>)snapshot.getValue();
+                    Log.d("myTAG", "onDataChange: Name: " + data.get("Name"));
+                    Log.d("myTAG", "onDataChange: Age: " + data.get("Age"));
+                    Log.d("myTAG", "onDataChange: Key: " + snapshot.getKey());
+                }
             }
 
             @Override
@@ -65,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference();
-        myRef.child("users").addValueEventListener(mListner);
+        myRef.addValueEventListener(mListner);
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -114,6 +130,43 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
                         Log.w((String) TAG, "Failed to read value.", databaseError.toException());
+                    }
+                });
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> updatedValues = new HashMap<>();
+
+                updatedValues.put("/users/name","Saleem");
+                updatedValues.put("/users/age",14);
+                updatedValues.put("/-LwNFPtal6s_N0_kOWXY/name","Gulraiz");
+                updatedValues.put("/-LwNFPtal6s_N0_kOWXY/age",26);
+
+                myRef.updateChildren(updatedValues);
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // myRef.child("users").child("name").setValue(null);
+
+                final Task<Void> task = myRef.child("users").removeValue();
+
+                task.addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+                    }
+                });
+
+                task.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
                     }
                 });
             }
