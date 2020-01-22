1f0a582fce654a81180927b5a1fbf3b1ae8c9acc
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2020-01-22 15:05:57 +0500

    ecommerce project 2

--- eCommerceApp/eCommerceApp/app/src/main/java/pk/com/aptech/ecommerceapp/SiginInFragment.java
+++ eCommerceApp/eCommerceApp/app/src/main/java/pk/com/aptech/ecommerceapp/SiginInFragment.java
@@ -3,11 +3,16 @@ package pk.com.aptech.ecommerceapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 
 /**
@@ -20,12 +25,33 @@ public class SiginInFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    private TextView dontHaveAnAccount;
+    private FrameLayout parentFrameLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sigin_in, container, false);
+        View view =  inflater.inflate(R.layout.fragment_sigin_in, container, false);
+        dontHaveAnAccount = view.findViewById(R.id.signin_tvDontHaveAccount);
+        parentFrameLayout = getActivity().findViewById(R.id.register_frameLayout);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+       dontHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               setTargetFragment(new SignUpFragment());
+           }
+       });
+    }
+
+    private void setTargetFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_right, R.anim.slideout_from_left);
+        fragmentTransaction.replace(parentFrameLayout.getId(), fragment);
+        fragmentTransaction.commit();
+    }
 }
--- eCommerceApp/eCommerceApp/app/src/main/java/pk/com/aptech/ecommerceapp/SignUpFragment.java
+++ eCommerceApp/eCommerceApp/app/src/main/java/pk/com/aptech/ecommerceapp/SignUpFragment.java
@@ -3,11 +3,16 @@ package pk.com.aptech.ecommerceapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 
 /**
@@ -15,7 +20,8 @@ import android.view.ViewGroup;
  */
 public class SignUpFragment extends Fragment {
 
-
+    private TextView alreadyHaveAnAccount;
+    FrameLayout parentFrameLayout;
     public SignUpFragment() {
         // Required empty public constructor
     }
@@ -25,7 +31,29 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view =  inflater.inflate(R.layout.fragment_sign_up, container, false);
+        alreadyHaveAnAccount = view.findViewById(R.id.signup_tvAlreadyHaveAccount);
+        parentFrameLayout = getActivity().findViewById(R.id.register_frameLayout);
+        return  view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        alreadyHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTargetFragment(new SiginInFragment());
+            }
+        });
+    }
+
+    private void setTargetFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_left, R.anim.slideout_from_right);
+        fragmentTransaction.replace(parentFrameLayout.getId(), fragment);
+        fragmentTransaction.commit();
+    }
 }
