b23f0131eb3ed1526f363b4831d024a282d58e2f
Gulraiz Gulshan <gulraizgulshan@gmail.com>
2020-01-30 00:27:35 +0500

    update main activity

--- eCommerceApp/eCommerceApp/app/src/main/java/pk/com/aptech/ecommerceapp/MainActivity.java
+++ eCommerceApp/eCommerceApp/app/src/main/java/pk/com/aptech/ecommerceapp/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
